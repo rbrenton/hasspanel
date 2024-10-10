b0898a456937025c9355bbeefd26aca24e48cd41
Brenton Strickler <rbrenton@gmail.com>
2024-10-09 22:21:42 -0400

    add map_to_rows

--- config.php
+++ config.php
@@ -1,84 +1,55 @@
 <?php
 require_once('icons.php');
 
+function map_to_rows($T, $D) {
+  global $rows, $icons;
+
+  foreach ($D as $arr) {
+    $row = $T;
+    foreach ($row as $k => $v) {
+      preg_match_all('/%([0-9]+)%/', $v, $m);
+      foreach ($m[0] as $i => $j) 
+        $v = preg_replace("/%{$m[1][$i]}%/", $arr[$m[1][$i]], $v);
+      if ($k == 'icon') $v = $icons[$v];
+      $row[$k] = $v;
+    }
+    $rows[] = $row;
+  }
+}
+
+// panel
 $device = array(
   'node' => 'indicator1',
   'banner' => 'StricklerHome',
 );
 
-$rows[] = array(
-  'icon' => $icons['coach-lamp'],
-  'label' => 'Front Porch Lights',
+// switches
+map_to_rows([
+  'icon' => '%0%',
+  'label' => '%1%',
   'type' => 'switch',
   'default' => '0',
   'on_value' => 'On',
   'off_value' => 'Off',
-  'entity' => 'switch.togglelinc_on_off_46_e4_15',
-);
+  'entity' => 'switch.%2%',
+], array(
+  [ 'coach-lamp'       , 'Front Porch Light', 'togglelinc_an_off_46_e4_15'  ],
+  [ 'outdoor-lamp'     , 'Driveway Light'   , 'togglelinc_an_off_46_e5_13'  ],
+));
 
-/*
-$rows[] = array(
-  'icon' => $icons['lamp'],
-  'label' => 'Brenton Dresser Light',
-  'type' => 'switch',
-  'default' => '0',
-  'on_value' => 'On',
-  'off_value' => 'Off',
-  'entity' => 'light.brenton_dresser',
-);
-
-$rows[] = array(
-  'icon' => $icons['floor-lamp'],
-  'label' => 'Office Lights',
-  'type' => 'switch',
-  'default' => '0',
-  'on_value' => 'On',
-  'off_value' => 'Off',
-  'entity' => 'light.dining_room',
-);
-*/
-
-$rows[] = array(
-  'icon' => $icons['garage-variant'],
-  'label' => 'Garage Door',
+// sensors
+map_to_rows([
+  'icon' => '%0%',
+  'label' => '%1%',
   'type' => 'text',
   'default' => 'n/a',
-  'entity' => 'sensor.garage_door',
-);
-
-$rows[] = array(
-  'icon' => $icons['outdoor-lamp'],
-  'label' => 'Driveway Light',
-  'type' => 'switch',
-  'default' => '0',
-  'on_value' => 'On',
-  'off_value' => 'Off',
-  'entity' => 'switch.togglelinc_on_off_46_e5_13',
-);
-
-$rows[] = array(
-  'icon' => $icons['lightning-bolt'],
-  'label' => 'Tesla Charger',
-  'type' => 'text',
-  'default' => 'n/a',
-  'entity' => 'sensor.tesla_wall_connector_status',
-);
-
-$rows[] = array(
-  'icon' => $icons['trash-can-outline'],
-  'label' => 'Trash Service',
-  'type' => 'text',
-  'default' => 'n/a',
-  'entity' => 'sensor.garbage_pickup_desc',
-);
-
-$rows[] = array(
-  'icon' => $icons['recycle-variant'],
-  'label' => 'Recycling Service',
-  'type' => 'text',
-  'default' => 'n/a',
-  'entity' => 'sensor.recycling_pickup_desc',
-);
+  'entity' => 'sensor.%2%',
+], array(
+  [ 'garage-variant'   , 'Garage Door'      , 'garage_door'                 ],
+  [ 'lightning-bolt'   , 'Tesla Charger'    , 'tesla_wall_connector_status' ],
+  [ 'trash-can-outline', 'Trash Service'    , 'garbage_pickup_desc'         ],
+  [ 'recycle-variant'  , 'Recycling Service', 'recycling_pickup_desc'       ],
+));
 
 /*
 $rows[] = array(
